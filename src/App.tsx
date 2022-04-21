823cee161ed922070bb5277f078995748b826837
handou2 <2864466519@qq.com>
2022-04-21 15:04:16 +0800

    add:css and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./assets/images/logo.svg";
+import styles from "./App.module.css";
+import robots from "./mockdata/robots.json";
+import Robot from "./components/Robot";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <div className={styles.app}>
+        <div className={styles.appHeader}>
+          <img src={logo} className={styles.appLogo} alt="logo" />
+          <h1>pig_nemo购物平台听说名字够长有傻子会看完</h1>
+        </div>
+        <div className={styles.robotList}>
+          {robots.map((r) => (
+            <Robot id={r.id} email={r.email} name={r.name} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
